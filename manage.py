089813cd56b5e218938ba34e6fa218e22948deb5
RToramaru <rafael.almeida.soares2012@gmal.com>
2024-10-22 22:10:56 -0300

    corrigir gerar planilha

--- manage.py
+++ manage.py
@@ -1,20 +1,36 @@
 import os
 import sys
+import io
+import webbrowser
+import threading
+from django.core.management import execute_from_command_line
 
 
-def main():
-    """Run administrative tasks."""
+def run_server():
+    """Run the Django server."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nepsui.settings')
+
+    # Redefine sys.stdout and sys.stderr
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
     try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(["manage.py", "runserver", "--noreload"])
+        execute_from_command_line(["manage.py", "runserver", "--noreload"])
+    except Exception as e:
+        sys.stderr.write(f"Error: {e}\n")
+
+
+def open_browser():
+    """Open the default browser with the Django server URL."""
+    import time
+    time.sleep(5)  # Wait for the server to start
+    webbrowser.open("http://127.0.0.1:8000")
 
 
 if __name__ == '__main__':
-    main()
+    # Start the server in a separate thread
+    server_thread = threading.Thread(target=run_server)
+    server_thread.start()
+
+    # Open the browser
+    # open_browser()
--- nepsui/settings.py
+++ nepsui/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-spmjcltkmjdtgg-zq)gay(b&fbt1s4p(&$vzg9&loq1#s8s51*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
